64e9c740d66060e11af3c2aac216684bbf2f5b2a
Alexander <pchp250g8@tutamail.com>
2026-08-30 21:24:36 +0300

    Fixed logical errors

--- Task-1-1-en.py
+++ Task-1-1-en.py
@@ -1,9 +1,9 @@
 # --coding:utf-8--
 print("Enter the number of blocks")
 n = int(input())
-print("Enter the number of towers")
+print("Enter the number of blocks for tower")
 k = int(input())
-t = n // k # Maximum number of towers
-b = t * k # Number of blocks
+t = n // k  # Maximum number of towers
+b = t * k  # Number of blocks
 print(f"From {n} blocks, you can build a maximum of {t} towers")
 print(f"This will take {b} blocks.")
